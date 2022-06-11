ae7ea39565506d5f08db6b3a83d9413dc604a51a
lqphuc123 <phuclequang2006@gmail.com>
2022-06-12 03:24:39 +0700

    Update popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -5,7 +5,14 @@ document.getElementById("checkbox").addEventListener("change", (event)=>{
 	console.log(cb.checked);
 	language = new String("en");
 	if (cb.checked) {
-	language = new String("vi");
+		language = new String("vi");
 	}
 	document.getElementById("output").innerHTML = language;
 });
+
+function clicked() {
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+		let url = tabs[0].url;
+		// use `url` here inside the callback because it's asynchronous!
+	});
+}
