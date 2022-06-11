d8dc8d417326c4f1998b688389a716b1252a59f5
lqphuc123 <phuclequang2006@gmail.com>
2022-06-12 01:56:40 +0700

    Update popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -1,11 +1,11 @@
 var language = new String("en");
 
 document.getElementById("checkbox").addEventListener("change", (event)=>{
-  const cb = document.querySelector('#checkbox');
-  console.log(cb.checked);
-  language = new String("en");
-  if (cb.checked) {
-    language = new String("vi");
-  }
-  document.getElementById("output").innerHTML = language;
+	const cb = document.querySelector('#checkbox');
+	console.log(cb.checked);
+	language = new String("en");
+	if (cb.checked) {
+	language = new String("vi");
+	}
+	document.getElementById("output").innerHTML = language;
 });
