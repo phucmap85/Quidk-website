fd4651f8b2eb72c49ee17cc4a01ad34d053dca7c
lqphuc123 <phuclequang2006@gmail.com>
2022-06-15 12:31:57 +0700

    Upload TextRank algorithm for Vietnamese

---
+++ extension/summarization.php
@@ -0,0 +1,19 @@
+<?php 
+include_once('TextRankFacade.php');
+include_once('StopWords/Vietnamese.php');
+include_once('Tool/Parser.php');
+
+header('Content-Type: application/json');
+
+$text = $_POST['text'];
+
+$tr = new TextRankFacade();
+
+$stopWords = new Vietnamese();
+$tr->setStopWords($stopWords);
+
+// Array of the sentences from the most important part of the text:
+$result = $tr->getHighlights($text); 
+
+echo json_encode($result);
+?>
