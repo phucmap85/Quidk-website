16380a313a3a40e68a8c3c5919c7c978995069f7
lqphuc123 <phuclequang2006@gmail.com>
2022-06-25 02:58:19 +0700

    Update 7 languages

--- extension/summarization.php
+++ extension/summarization.php
@@ -1,9 +1,18 @@
 <?php
     require_once('vendor/autoload.php');
     include_once('TextRank/TextRankFacade.php');
-    include_once('TextRank/StopWords/Vietnamese.php');
-    include_once('TextRank/StopWords/English.php');
     include_once('TextRank/Tool/Parser.php');
+	
+	include_once('TextRank/StopWords/Vietnamese.php');
+    include_once('TextRank/StopWords/English.php');
+	include_once('TextRank/StopWords/French.php');
+	include_once('TextRank/StopWords/German.php');
+	include_once('TextRank/StopWords/Indonesian.php');
+	include_once('TextRank/StopWords/Italian.php');
+	include_once('TextRank/StopWords/Norwegian.php');
+	include_once('TextRank/StopWords/Russian.php');
+	include_once('TextRank/StopWords/Spanish.php');
+	
     header('Content-Type: application/json; charset=utf-8');
 
     $url = $_POST['text'];
@@ -23,12 +32,19 @@
 
     $tr = new TextRankFacade();
 
-    $detector = new LanguageDetector\LanguageDetector(null, ['en', 'vi']);
+    $detector = new LanguageDetector\LanguageDetector(null, ['vi', 'en', 'fr', 'de', 'id', 'it', 'no', 'ru', 'es']);
 
     $lang = $detector->evaluate($text);
 
     $stopWords = new Vietnamese();
     if($lang == 'en') $stopWords = new English();
+	if($lang == 'fr') $stopWords = new French();
+	if($lang == 'de') $stopWords = new German();
+	if($lang == 'id') $stopWords = new Indonesian();
+	if($lang == 'it') $stopWords = new Italian();
+	if($lang == 'no') $stopWords = new Norwegian();
+	if($lang == 'ru') $stopWords = new Russian();
+	if($lang == 'es') $stopWords = new Spanish();
 
     $tr->setStopWords($stopWords);
     $result = $tr->getHighlights($text);
